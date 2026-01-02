d271b2bd44bf1f22d8d0586fef992781c21419b9
vincenciusadyatma <adyatmav@gmail.com>
2026-01-02 14:13:43 +0700

    feat: create configuration for database

---
+++ src/config/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config.config import Config
+
+engine = create_engine(Config.DB_URL, echo=True, future=True)
+Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
