e8d34af90938db5583e604f2b719c004ee160eca
vincenciusadyatma <adyatmav@gmail.com>
2026-02-19 21:43:46 +0700

    feat: create migrations for users and news

--- src/models/News.py
+++ src/models/News.py
@@ -1,8 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-
-Base = declarative_base()
+from src.config import Base
 
 class News(Base):
     __tablename__ = "news"
